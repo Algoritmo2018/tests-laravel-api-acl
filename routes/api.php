51c03753710d680401740aca78c3b648de3f29ac
Luis Chilembo Mateus <luischilembomateus@gmail.com>
2024-09-05 18:44:34 +0100

    test:should return users page 2

--- routes/api.php
+++ routes/api.php
@@ -6,11 +6,11 @@ use App\Http\Controllers\Api\PermissionUserController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/me', [AuthApiController::class, 'me'])->name('auth.me')->middleware('auth:sanctum');
-Route::post('/logout', [AuthApiController::class, 'logout'])->name('auth.logout')->middleware('auth:sanctum');
 Route::post('/auth', [AuthApiController::class, 'auth'])->name('auth.login');
 Route::post('/users', [UserController::class, 'store'])->name('users.store');
 Route::middleware(['auth:sanctum', 'acl'])->group(function () {
+    Route::get('/me', [AuthApiController::class, 'me'])->name('auth.me')->middleware('auth:sanctum');
+    Route::post('/logout', [AuthApiController::class, 'logout'])->name('auth.logout')->middleware('auth:sanctum');
     Route::apiResource('/permissions', PermissionController::class);
 
     Route::get('/users/{user}/permissions', [PermissionUserController::class, 'getPermissionsOfUser'])->name('users.permissions');
@@ -18,7 +18,6 @@ Route::middleware(['auth:sanctum', 'acl'])->group(function () {
     Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
     Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
     Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
-
     Route::get('/users', [UserController::class, 'index'])->name('users.index');
 });
 
---
+++ tests/Feature/Api/UserApiTest.php
@@ -0,0 +1,85 @@
+<?php
+
+use App\Models\User;
+use function Pest\Laravel\getJson;
+use function Pest\Laravel\postJson;
+use App\Http\Middleware\ACLMiddleware;
+use function Pest\Laravel\withoutMiddleware;
+
+beforeEach(function () {
+    withoutMiddleware(ACLMiddleware::class);
+    $this->user = User::factory()->create();
+    $this->token = $this->user->createToken('test_e2e')->plainTextToken;
+});
+
+test('should return 200', function () {
+    getJson(
+        route('users.index'),
+        [
+            'Authorization' => 'Bearer ' . $this->token
+        ]
+    )->assertJsonStructure([
+        'data' => [
+            '*' => [
+                'id',
+                'name',
+                'email',
+                'permissions' => []
+            ]
+        ]
+    ])->assertOk();
+});
+
+test('should return 200 - with many users', function () {
+    User::factory()->count(20)->create();
+    $response = getJson(
+        route('users.index'),
+        [
+            'Authorization' => 'Bearer ' . $this->token
+        ]
+    )->assertJsonStructure([
+        'data' => [
+            '*' => [
+                'id',
+                'name',
+                'email',
+                'permissions' => []
+            ]
+        ],
+        'meta' => [
+            'total',
+            'current_page',
+            'from',
+            'last_page',
+            'links' => [],
+            'path',
+            'per_page',
+            'to'
+        ]
+    ])->assertOk();
+
+    expect(count($response['data']))->toBe(15);
+    expect($response['meta']['total'])->toBe(21);
+});
+
+test('should return users page 2', function () {
+    User::factory()->count(22)->create();
+    $response = getJson(
+        route('users.index').'?page=2',
+        [
+            'Authorization' => 'Bearer ' . $this->token
+        ]
+    )->assertJsonStructure([
+        'data' => [
+            '*' => [
+                'id',
+                'name',
+                'email',
+                'permissions' => []
+            ]
+        ],
+        'meta' => ['total', 'current_page', 'from', 'last_page', 'links' => [], 'path', 'per_page', 'to']
+    ])->assertOk();
+    expect(count($response['data']))->toBe(8);
+    expect($response['meta']['total'])->toBe(23);
+});
